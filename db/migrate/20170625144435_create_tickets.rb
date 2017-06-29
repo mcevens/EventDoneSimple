62e767a1cae7cb9c25a5c37d8e0887cd2dffdee8
Mc Evens <mcevens.bienaime@gmail.com>
2017-06-29 06:42:17 -0400

    Fix db

--- db/migrate/20170625144435_create_tickets.rb
+++ db/migrate/20170625144435_create_tickets.rb
@@ -1,7 +1,6 @@
 class CreateTickets < ActiveRecord::Migration[5.0]
   def change
     create_table :tickets do |t|
-      t.string :name
       t.integer :event_id
       t.integer :ticket_type_id
       t.decimal :price, precision: 5, scale: 2
