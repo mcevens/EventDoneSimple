b31a2a0878cf8b422d1eb6f8daa72722a382cf34
Mc Evens <mcevens.bienaime@gmail.com>
2017-06-29 20:39:27 -0400

    Change ticket_actions.js

--- frontend/actions/ticket_actions.js
+++ frontend/actions/ticket_actions.js
@@ -1,6 +1,14 @@
 export const RECEIVE_NEW_TICKET = 'RECEIVE_NEW_TICKET';
+export const REQUEST_TICKETS = 'REQUEST_TICKETS';
 
 export const receiveNewTicket = (event, ticket) => ({
   type: RECEIVE_NEW_TICKET,
   ticket
 });
+
+export const requestTickets = (currentUser) => {
+  return {
+    type: REQUEST_TICKETS,
+    currentUser
+  };
+};
