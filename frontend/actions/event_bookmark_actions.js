25f9683691253a42f02ab67d25045dcf70c0b6f1
Mc Evens <mcevens.bienaime@gmail.com>
2017-06-29 20:38:52 -0400

    Add bookmark action

--- frontend/actions/event_bookmark_actions.js
+++ frontend/actions/event_bookmark_actions.js
@@ -1,6 +1,8 @@
 import * as APIUtil from '../util/event_bookmark_util.js';
 export const RECEIVE_EVENT_BOOKMARKS = 'RECEIVE_EVENT_BOOKMARKS';
 export const RECEIVE_NEW_BOOKMARK = 'RECEIVE_NEW_BOOKMARK';
+export const REQUEST_BOOKMARKS = 'REQUEST_BOOKMARKS';
+// import receiveAllEvent from '../event/';
 
 export const receiveEventBookMarks = eventBookmarks => {
   return ({
@@ -14,16 +16,23 @@ export const requestAllEventBookMark = () => (dispatch) => {
   .then(eventBookmarks => dispatch(receiveEventBookMarks(eventBookmarks)));
 };
 
-export const createBookmark = bookmark => dispatch => {
- return APIUtil.createEventBookmark(bookmark).then(events => {
-    dispatch(receiveAllEvent(events));
-    return bookmark;
-  }).fail(err => console.log(err));
-};
+// export const createBookmark = bookmark => dispatch => {
+//  return APIUtil.createEventBookmark(bookmark).then(events => {
+//     dispatch(receiveAllEvent(events));
+//     return bookmark;
+//   }).fail(err => console.log(err));
+// };
 
 export const receiveNewBookmark = bookmark => {
 return ({
   type: RECEIVE_NEW_BOOKMARK,
   bookmark
 });
+};
+
+export const requestBookmarks = (currentUser) => {
+  return {
+    type: REQUEST_BOOKMARKS,
+    currentUser
+  };
 };
