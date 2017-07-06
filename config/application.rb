515111fe73d5285aab06144174337a9486d8c080
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-06 12:05:03 -0400

    Change application.rb for test

--- config/application.rb
+++ config/application.rb
@@ -24,15 +24,28 @@ module EventDoneSimple
     # }
 
 
+    # config.paperclip_defaults = {
+    #   :storage => :s3,
+    #   :s3_protocol => :https,
+    #   :s3_region => ENV["s3_region"],
+    #   :s3_credentials => {
+    #     :bucket => ENV["s3_bucket"],
+    #     :access_key_id => ENV["s3_access_key_id"],
+    #     :secret_access_key => ENV["s3_secret_access_key"],
+    #     :region => ENV['AWS_REGION'],
+    #     :s3_host_name => "s3-#{ENV["s3_region"]}.amazonaws.com",
+    #     :url => ":s3_host_name"
+    #   }
+    # }
+
+
     config.paperclip_defaults = {
       :storage => :s3,
-      :s3_protocol => :https,
-      :s3_region => ENV["s3_region"],
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
-        :region => ENV['AWS_REGION'],
+        :s3_region => ENV["s3_region"],
         :s3_host_name => "s3-#{ENV["s3_region"]}.amazonaws.com",
         :url => ":s3_host_name"
       }
