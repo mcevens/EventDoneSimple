cc6d913d6126aabbe5fb51df2803ba83280bd6d8
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-06 12:52:20 -0400

    Change paperclier configuration

--- config/application.rb
+++ config/application.rb
@@ -11,32 +11,6 @@ module EventDoneSimple
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.paperclip_defaults = {
-    #   :storage => :s3,
-    #   :s3_credentials => {
-    #     :bucket => ENV["s3_bucket"],
-    #     :access_key_id => ENV["s3_access_key_id"],
-    #     :secret_access_key => ENV["s3_secret_access_key"],
-    #     :s3_region => ENV["s3_region"],
-    #     :s3_host_name => "s3-#{ENV["s3_region"]}.amazonaws.com",
-    #     :url => ":s3_host_name"
-    #   }
-    # }
-
-
-    # config.paperclip_defaults = {
-    #   :storage => :s3,
-    #   :s3_protocol => :https,
-    #   :s3_region => ENV["s3_region"],
-    #   :s3_credentials => {
-    #     :bucket => ENV["s3_bucket"],
-    #     :access_key_id => ENV["s3_access_key_id"],
-    #     :secret_access_key => ENV["s3_secret_access_key"],
-    #     :region => ENV['AWS_REGION'],
-    #     :s3_host_name => "s3-#{ENV["s3_region"]}.amazonaws.com",
-    #     :url => ":s3_host_name"
-    #   }
-    # }
 
     config.paperclip_defaults = {
       :storage => :s3,
@@ -51,8 +25,6 @@ module EventDoneSimple
         :url => ":s3_host_name"
       }
     }
-
-
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
   config.active_record.raise_in_transactional_callbacks = true
